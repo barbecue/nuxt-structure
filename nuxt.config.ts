3b2bc6f155593593507c77b528b4b255b1ff5ee9
BBQ <barbeku@pm.me>
2023-08-12 01:23:03 +0300

    chore: Added packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  components: ['~/components', '~/components/layouts'],
+  css: ['~/stylesheets/main.scss'],
+  modules: [
+      '@nuxtjs/tailwindcss',
+      '@nuxtjs/eslint-module',
+      '@nuxtjs/google-fonts',
+      '@nuxt/image',
+      'nuxt-icon'
+  ],
+  googleFonts: {
+      download: true,
+      preconnect: true,
+      prefetch: true,
+      families: {
+          'Archivo+Black': true,
+      },
+  },
 })
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        archivo: ['Archivo Black', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio')
+  ]
+}
